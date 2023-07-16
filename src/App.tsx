419319e0433b29e84bc01de6474371f5e46643e8
seorlanlee <leesugar90@gmail.com>
2023-07-16 13:20:28 +0900

    useCallback: 사용법

--- src/App.tsx
+++ src/App.tsx
@@ -1,40 +1,27 @@
-import { useEffect, useMemo, useState } from 'react';
+import { useCallback, useEffect, useState } from 'react';
 
 function App() {
   const [number, setNumber] = useState(0);
-  const [isKorea, setIsKorea] = useState(true);
+  const [toggle, setToggle] = useState(true);
 
-  // 원시 type일 때는 문제 없음
-  // const location = isKorea ? '한국' : '외국';
-
-  // 객체 type일 경우
-  // const location = {
-  //   country: isKorea ? '한국' : '외국',
-  // };
-
-  const location = useMemo(() => {
-    return {
-      country: isKorea ? '한국' : '외국',
-    };
-  }, [isKorea]);
+  const someFunction = useCallback(() => {
+    return console.log(`someFunc: number: ${number}`);
+  }, [number]);
 
   useEffect(() => {
-    console.log('useEffect 호출');
-    // 오래걸리는 작업..
-  }, [location]);
+    console.log('someFunction이 변경되었습니다.');
+  }, [someFunction]);
 
   return (
     <>
-      <h2>하루에 몇끼 먹어요?</h2>
       <input
         type="number"
         value={number}
         onChange={e => setNumber(+e.target.value)}
       />
-      <hr />
-      <h2>어느 나라에서 왔어요?</h2>
-      <p>나라: {location.country}</p>
-      <button onClick={() => setIsKorea(!isKorea)}>비행기 타자</button>
+      <button onClick={() => setToggle(!toggle)}>{toggle.toString()}</button>
+      <br />
+      <button onClick={someFunction}>Call somefunc</button>
     </>
   );
 }

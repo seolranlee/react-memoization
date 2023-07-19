708d4411a2ee485b56e6840129e4b0870fd14a45
seorlanlee <leesugar90@gmail.com>
2023-07-19 10:18:28 +0900

    Automatic Batching

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,27 @@
-import { type FC, useCallback, useState } from 'react';
-import Todo from './Todo';
+import { type FC, useState, useEffect } from 'react';
 
 const App: FC = () => {
-  const [id, setId] = useState(1);
+  const [teamA, setTeamA] = useState(10);
+  const [teamB, setTeamB] = useState(10);
+
+  useEffect(() => {
+    if (teamA !== teamB) alert('줄다리기 종료');
+  }, [teamA, teamB]);
+
+  const delay = (ms: number) => new Promise(resolve => setTimeout(resolve, ms));
+
+  const decrease = async () => {
+    await delay(1500);
+    setTeamA(a => a - 1);
+    setTeamB(b => b - 1);
+  };
 
   return (
     <>
-      <input type="number" value={id} onChange={e => setId(+e.target.value)} />
-      <Todo id={id} />
+      <h1>줄다리기는 균형이 중요해!</h1>
+      <strong>A: {teamA}</strong>
+      <strong>B: {teamB}</strong>
+      <button onClick={() => void decrease()}>한명 줄이기</button>
     </>
   );
 };
